89a3477bfc9eb5693d64f3b1e4b3f25973d81ee1
Tad Smith <tadsmith@hotmail.com>
2022-03-10 16:38:35 -0600

    Additional subscriber tracking.

--- trunk-recorder/main.cc
+++ trunk-recorder/main.cc
@@ -830,7 +830,7 @@ void print_status() {
       for (std::vector<SubscriberData>::iterator it = talkgroup.second.begin(); it != talkgroup.second.end();) {
         SubscriberData subscriber = *it;
         tgs = tgs + std::to_string(subscriber.suid) + ",";
-        BOOST_LOG_TRIVIAL(error) << "SUID: " << subscriber.suid << " Last Activity: " << localtime(subscriber.last_activity) << ".";
+        BOOST_LOG_TRIVIAL(error) << "SUID: " << subscriber.suid << " Last Activity: " << localtime(&subscriber.last_activity) << ".";
         it++;
       }
       //Hack to remove last comma
