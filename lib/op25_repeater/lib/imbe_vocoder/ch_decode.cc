c3a53fc6b65a782fdf8b9fa681633d9aee7267b7
Tad Smith <tadsmith@hotmail.com>
2022-12-01 12:34:50 -0600

    Better math.

--- lib/op25_repeater/lib/imbe_vocoder/ch_decode.cc
+++ lib/op25_repeater/lib/imbe_vocoder/ch_decode.cc
@@ -88,13 +88,13 @@ void decode_frame_vector(IMBE_PARAM *imbe_param, Word16 *frame_vector, Word16 *p
 
 	// 6.1 FUNDAMENTAL FREQUENCY ENCODING AND DECODING
 	// ALGORITHM 46
-	imbe_param->fund_freq = (UWord32)((4 * M_PI) / (imbe_param->b_vec[0]) + 39.5);
+	imbe_param->fund_freq = (4 * M_PI) / (imbe_param->b_vec[0] + 39.5);
 	fprintf(stderr, "Made it past the FF. Mine: %d\t Theirs: %d\tB0: %d\n", imbe_param->fund_freq,L_add(imbe_param->fund_freq, L_tmp),imbe_param->b_vec[0]);
 
 	// 6.1 FUNDAMENTAL FREQUENCY ENCODING AND DECODING
 	// ALGORITHM 47
-	imbe_param->num_harms = (UWord32)floor(.9254 * floor((M_PI/imbe_param->fund_freq)+.25));
-	fprintf(stderr, "Made it past the num harms.\n");
+	imbe_param->num_harms = floor(.9254 * floor((M_PI/imbe_param->fund_freq)+.25));
+	fprintf(stderr, "Made it past the num harms. %d\n", imbe_param->num_harms);
 
 	if(imbe_param->num_harms <= 36)
 		imbe_param->num_bands = extract_h((UWord32)(imbe_param->num_harms + 2) * CNST_0_33_Q0_16);   // fix((L+2)/3)
