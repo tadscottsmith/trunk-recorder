b529d07d1502c95239af2fb365db5e51ac1e6088
Tad Smith <tadsmith@hotmail.com>
2022-12-01 20:05:36 -0600

    Closer to normal.

--- lib/op25_repeater/lib/imbe_vocoder/decode.cc
+++ lib/op25_repeater/lib/imbe_vocoder/decode.cc
@@ -87,7 +87,7 @@ void imbe_vocoder::decode(IMBE_PARAM *imbe_param, Word16 *frame_vector, Word16 *
     }
   } else {
     for (j = 0; j < FRAME; j++) {
-      snd[j] = add(snd[j] * 4, snd_tmp[j]); // Attempting to increase volume.
+      snd[j] = add(snd[j], snd_tmp[j]);
     }
   }
 
