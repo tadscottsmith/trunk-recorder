9adb4860989e0c09e6115ab799ab9be06be0fab2
Tad Smith <tadsmith@hotmail.com>
2022-12-08 18:47:27 -0600

    Syntax.

--- lib/op25_repeater/lib/rust_imbe_decoder.cc
+++ lib/op25_repeater/lib/rust_imbe_decoder.cc
@@ -3,30 +3,5 @@
 #include "rust_imbe_decoder.h"
 
 rust_imbe_decoder::rust_imbe_decoder() {
-  int i, j;
-  // initialize
-  ER = 0;
-  rpt_ctr = 0;
-  OldL = 0;
-  L = 9;
-  Old = 1;
-  New = 0;
-  psi1 = 0.0;
-  for (i = 0; i < 58; i++) {
-    for (j = 0; j < 2; j++) {
-      log2Mu[i][j] = 0.0;
-    }
-  }
-  for (i = 0; i < 57; i++) {
-    for (j = 0; j < 2; j++) {
-      phi[i][j] = 0.0;
-    }
-  }
-  for (i = 0; i < 256; i++) {
-    Olduw[i] = 0.0;
-  }
-  u[0] = 3147;
-  for (i = 1; i < 211; i++) {
-    u[i] = next_u(u[i - 1]);
-  }
+
 }
--- lib/op25_repeater/lib/rust_imbe_decoder.h
+++ lib/op25_repeater/lib/rust_imbe_decoder.h
@@ -6,51 +6,45 @@
 
 #include <stdint.h>
 
-
 class rust_imbe_decoder : public imbe_decoder {
 public:
-
-	rust_imbe_decoder();
-	virtual ~rust_imbe_decoder();
-
+  rust_imbe_decoder();
+  virtual ~rust_imbe_decoder();
 
 private:
+  // NOTE: Single-letter variable names are upper case only; Lower
+  //				  case if needed is spelled. e.g. L, ell
 
-	//NOTE: Single-letter variable names are upper case only; Lower
-	//				  case if needed is spelled. e.g. L, ell
+  float ER;    // BER Estimate
+  int rpt_ctr; // Frame repeat counter
 
-	float ER;					// BER Estimate
-	int rpt_ctr;				// Frame repeat counter
+  int bee[58];     // Encoded Spectral Amplitudes
+  float M[57][2];  // Enhanced Spectral Amplitudes
+  float Mu[57][2]; // Unenhanced Spectral Amplitudes
+  int vee[57][2];  // V/UV decisions
+  float suv[160];  // Unvoiced samples
+  float sv[160];   // Voiced samples
+  float log2Mu[58][2];
+  float Olduw[256];
+  float psi1;
+  float phi[57][2];
+  uint32_t u[211];
 
-	int bee[58];				// Encoded Spectral Amplitudes
-	float M[57][2];				// Enhanced Spectral Amplitudes
-	float Mu[57][2];			// Unenhanced Spectral Amplitudes
-	int vee[57][2];				// V/UV decisions
-	float suv[160];				// Unvoiced samples
-	float sv[160];				// Voiced samples
-	float log2Mu[58][2];
-	float Olduw[256];
-	float psi1;
-	float phi[57][2];
-	uint32_t u[211];
+  int Old;
+  int New;
+  int L;
+  int OldL;
+  float w0;
+  float Oldw0;
+  float Luv; // number of unvoiced spectral amplitudes
 
-	int Old;
-	int New;
-	int L;
-	int OldL;
-	float w0;
-	float Oldw0;
-	float Luv;						//number of unvoiced spectral amplitudes
+  float TM = 20480;    // Amplitude threshold of current frame;
+  float OldTM = 20480; // Amplitude threshold of previous frame;
 
-	float TM = 20480;						//Amplitude threshold of current frame;
-	float OldTM = 20480;					//Amplitude threshold of previous frame;
-
-	char sym_b[4096];
-	char RxData[4096];
-	int sym_bp;
-	int ErFlag;
-
+  char sym_b[4096];
+  char RxData[4096];
+  int sym_bp;
+  int ErFlag;
 };
-
 
 #endif /* INCLUDED_SOFTWARE_IMBE_DECODER_H */
