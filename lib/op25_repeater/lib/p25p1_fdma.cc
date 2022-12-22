3d37d715ecb2dc7a2f57e7dfd2f9d9363af22076
Tad Smith <tadsmith@hotmail.com>
2022-12-22 11:19:19 -0600

    Move to software_decoder.decode().

--- lib/op25_repeater/lib/p25p1_fdma.cc
+++ lib/op25_repeater/lib/p25p1_fdma.cc
@@ -644,7 +644,7 @@ namespace gr {
                     imbe_deinterleave(A, cw, i);
                     uint16_t imbe_error = 0;
 
-                    errs = imbe_header_decode(cw, u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7], E0, E4, ET);
+                    errs = 0; //imbe_header_decode(cw, u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7], E0, E4, ET);
 
                     if (d_debug >= 9) {
                         packed_codeword p_cw;
@@ -696,7 +696,7 @@ namespace gr {
                             std::string encr = "{\"encrypted\": " + std::to_string(0) + ", \"algid\": " + std::to_string(ess_algid) + ", \"keyid\": " + std::to_string(ess_keyid) + "}";
                             send_msg(encr, M_P25_JSON_DATA);
                             // This is the Vocoder that OP25 currently uses.
-                            software_decoder.decode_fullrate(u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7], E0, ET);
+                            software_decoder.decode(cw);
                             audio_samples *samples = software_decoder.audio();
                             for (int i=0; i < SND_FRAME; i++) {
                            	    if (samples->size() > 0) {
